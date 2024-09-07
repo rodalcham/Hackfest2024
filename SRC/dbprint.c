174d92136c3e2c23659245ab1a14f00f06f3f332
Rodrigo Chávez <rodalcham@gmail.com>
2024-09-07 18:23:56 +0200

    added dbprint

--- SRC/dbprint.c
+++ SRC/dbprint.c
@@ -6,7 +6,7 @@
 /*   By: rchavez@student.42heilbronn.de <rchavez    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 16:59:09 by rchavez@stu       #+#    #+#             */
-/*   Updated: 2024/09/07 18:00:26 by rchavez@stu      ###   ########.fr       */
+/*   Updated: 2024/09/07 18:19:08 by rchavez@stu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,29 +36,30 @@ void	dbprint(emp_t *head, int argc, char **argv)
 		while (j < 5)
 		{
 			temp = head;
-			printf("\t\t{\n\t\t\tname: \"%s\"\n", Departments[j]);
+			printf("\t\t{\n\t\t\tname: \"%s\",\n\t\t\tstaff: [\n", Departments[j]);
 			while (temp)
 			{
 				if (!strcmp(temp->local, argv[i]) && !strcmp(temp->department, Departments[j]))
 				{
-					printf("\t\t\t\tid: %s\n", temp->id);
-					printf("\t\t\t\tname: \"%s\"\n", temp->name);
-					printf("\t\t\t\tlname: \"%s\"\n", temp->lname);
-					printf("\t\t\t\tVM: %f\n", temp->emmissions[0]);
-					printf("\t\t\t\tMensa: %f\n", temp->emmissions[1]);
-					printf("\t\t\t\tOfficeSupplies: %f\n", temp->emmissions[2]);
-					printf("\t\t\t\tParking: %f\n", temp->emmissions[3]);
-					printf("\t\t\t\tTravel: %f\n", temp->emmissions[4]);
-					printf("\t\t\t\tEmails: %f\n", temp->emmissions[5]);
-					printf("\t\t\t\tEnergy: %f\n", temp->emmissions[6]);
+					printf("\t\t\t\t{\n");
+					printf("\t\t\t\t\tname: \"%s\",\n", temp->name);
+					printf("\t\t\t\t\tlname: \"%s\",\n", temp->lname);
+					printf("\t\t\t\t\tVM: %f,\n", temp->emmissions[0]);
+					printf("\t\t\t\t\tMensa: %f,\n", temp->emmissions[1]);
+					printf("\t\t\t\t\tOfficeSupplies: %f,\n", temp->emmissions[2]);
+					printf("\t\t\t\t\tParking: %f,\n", temp->emmissions[3]);
+					printf("\t\t\t\t\tTravel: %f,\n", temp->emmissions[4]);
+					printf("\t\t\t\t\tEmails: %f,\n", temp->emmissions[5]);
+					printf("\t\t\t\t\tEnergy: %f,\n", temp->emmissions[6]);
+					printf("\t\t\t\t},\n");
 				}
 				temp = temp->next;
 			}
 			j++;
-			printf("\t\t}\n");
+			printf("\t\t\t],\n\t\t},\n");
 		}
-		printf("\t]\n");
-		printf("}\n");
+		printf("\t],\n");
+		printf("},\n");
 		i++;
 	}
 }
